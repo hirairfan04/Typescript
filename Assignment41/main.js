a3b9eeb2b28cd5b0821bbaf27a0ac60b84a6f7c6
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:33:02 +0500

    Add files via upload

---
+++ Assignment41/main.js
@@ -0,0 +1,7 @@
+var magician_names = ['John', 'Smith', 'Edward', 'Henry'];
+function show_magicians(magician_names) {
+    for (var i = 0; i < magician_names.length; i++) {
+        console.log(magician_names[i]);
+    }
+}
+show_magicians(magician_names);
