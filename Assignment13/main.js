004ed4e9a297c11e84a73f54974ec11c64f90e8b
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 22:48:54 +0500

    Add files via upload

---
+++ Assignment13/main.js
@@ -0,0 +1,5 @@
+var transportation = ['kia sportage', 'revo', 'scooter'];
+var message = ('I would like to own a ');
+console.log(message + transportation[0]);
+console.log(message + transportation[1]);
+console.log(message + transportation[2]);
---
+++ Assignment13/main.ts
@@ -0,0 +1,5 @@
+let transportation: string[]= ['kia sportage', 'revo', 'scooter'];
+let message=('I would like to own a ');
+console.log(message+transportation[0]);
+console.log(message+transportation[1]);
+console.log(message+transportation[2]);
