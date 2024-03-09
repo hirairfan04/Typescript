d377f28b07c47dce7a3afd7a633a93d5aa2b2aba
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 20:16:49 +0500

    Add files via upload

---
+++ Assignment06/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let firstName = 'Hira Irfan';
+console.log(`\t${firstName}\n`);
+console.log(`\t${firstName}\n`);
---
+++ Assignment06/main.ts
@@ -0,0 +1,3 @@
+let firstName='Hira Irfan';
+console.log(`\t${firstName}\n`);
+console.log(`\t${firstName}\n`);
