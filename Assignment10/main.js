0242a3563d99355867f37f45955cb1d99ca08678
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 20:33:21 +0500

    Add files via upload

---
+++ Assignment10/main.js
@@ -0,0 +1,4 @@
+"use strict";
+fvrt_number = 7; //declaring the favorite number
+message = 'My favorite number is'; //message referring to that number  
+console.log(message, fvrt_number); //printing the whole message
---
+++ Assignment10/main.ts
@@ -0,0 +1,3 @@
+let fvrt_number=7;                         //declaring the favorite number
+let message= 'My favorite number is';      //message referring to that number  
+console.log(`${message} ${fvrt_number}`);      //printing the whole message
