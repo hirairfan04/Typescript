152c436a3cc5e1fe3a8137bf0ff58f058852ab2d
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 22:52:43 +0500

    Add files via upload

---
+++ Assignment14/main.js
@@ -0,0 +1,5 @@
+var guest = ['Miss Sara', 'Miss Hurain', 'Miss Zoha'];
+var message = ',I would like to invite you at dinner at my place this Saturday at 7pm.';
+console.log(guest[0] + message);
+console.log(guest[1] + message);
+console.log(guest[2] + message);
