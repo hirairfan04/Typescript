0b4d68802ecb2f232c7b1fd4359969ebba81611e
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:15:29 +0500

    Add files via upload

---
+++ Assignment25/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let alien_color = 'green';
+let newcolor = 'green';
+if (newcolor == alien_color) {
+    console.log('You earned 5 points');
+}
+let Alien_color = 'green';
+let Newcolor = 'yellow';
+if (Newcolor == Alien_color) {
+    console.log('You earned 5 points');
+}
---
+++ Assignment25/main.ts
@@ -0,0 +1,13 @@
+let alien_color='green';
+let newcolor='green';
+if(newcolor==alien_color)
+{
+    console.log('You earned 5 points');
+}
+
+let Alien_color='green';
+let Newcolor='yellow';
+if(Newcolor==Alien_color)
+{
+    console.log('You earned 5 points');
+}
