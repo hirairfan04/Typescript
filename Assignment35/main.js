b8fae893481201cc08ce37fcca010c5f1b3ef12f
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:26:36 +0500

    Add files via upload

---
+++ Assignment35/main.js
@@ -0,0 +1,5 @@
+var animals = ['cat', 'dog', 'rabbit'];
+for (var i = 0; i < animals.length; i++) {
+    console.log("A ".concat(animals[i], " would make a great pet"));
+}
+console.log('Any of these pets would make a great pet!');
---
+++ Assignment35/main.ts
@@ -0,0 +1,6 @@
+let animals=['cat','dog','rabbit'];
+for(let i=0; i<animals.length; i++)
+{
+    console.log(`A ${animals[i]} would make a great pet`);
+}
+console.log('Any of these pets would make a great pet!');
