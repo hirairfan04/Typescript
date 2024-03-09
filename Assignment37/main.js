eab3675f2b91d2dae427f4bfbfba25d04d34b666
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:28:39 +0500

    Add files via upload

---
+++ Assignment37/main.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) { console.log("Size:".concat(size, ", Message:").concat(message)); }
+make_shirt('large', 'I love typescript');
+make_shirt('medium', 'I love typescript');
+function makeShirt(size, message) { console.log("Size:".concat(size, ", Message:").concat(message)); }
+makeShirt('medium', 'Do what makes you happy!');
---
+++ Assignment37/main.ts
@@ -0,0 +1,5 @@
+function make_shirt(size:string,message:string) {console.log(`Size:${size}, Message:${message}`)}
+make_shirt('large','I love typescript');
+make_shirt('medium','I love typescript');
+function makeShirt(size:string,message:string) {console.log(`Size:${size}, Message:${message}`)}
+makeShirt('medium','Do what makes you happy!');
