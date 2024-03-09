a54512f721d5b3d7f6e2c337abcc701577da245e
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:00:12 +0500

    Add files via upload

---
+++ Assignment16/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//let guest=['Miss Abrish','Miss Hurain','Miss Zoha'];
+let message = ',I would like to invite you at dinner at my place this Saturday at 7pm.';
+//console.log(guest[0]+message);
+//console.log(guest[1]+message);
+//console.log(guest[2]+message);
+console.log('Here is an announcemnt!!! We have a bigger table for dinner now');
+let guest = ['Miss Meerab', 'Miss Abrish', 'Miss Irtifa', 'Miss Hurain', 'Miss Zoha'];
+let quantity = guest.push('Miss Sana');
+console.log(guest[0] + message);
+console.log(guest[1] + message);
+console.log(guest[2] + message);
+console.log(guest[3] + message);
+console.log(guest[4] + message);
+console.log(guest[5] + message);
