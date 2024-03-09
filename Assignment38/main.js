662d405c5cad7092cf36dc85cdeb1f0ee8d2713f
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:29:37 +0500

    Add files via upload

---
+++ Assignment38/main.js
@@ -0,0 +1,5 @@
+function describe_city(city, country) { console.log("".concat(city, " is in ").concat(country)); }
+describe_city('Karachi', 'Pakistan');
+describe_city('Lahore', 'Pakistan');
+describe_city('Multan', 'Pakistan');
+describe_city('Seoul', 'South Korea');
---
+++ Assignment38/main.ts
@@ -0,0 +1,5 @@
+function describe_city(city:string,country:string) {console.log(`${city} is in ${country}`)}
+describe_city('Karachi','Pakistan');
+describe_city('Lahore','Pakistan');
+describe_city('Multan','Pakistan');
+describe_city('Seoul','South Korea');
