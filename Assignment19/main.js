58d75b8a33fa8ae3873b5302e9e2412d68d07efb
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:06:36 +0500

    Add files via upload

---
+++ Assignment19/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let guest = ['Miss Sara', 'Miss Hurain', 'Miss Zoha'];
+console.log(`We have ${guest.length} guests for dinner.`);
---
+++ Assignment19/main.ts
@@ -0,0 +1,2 @@
+let guest:string[]=['Miss Sara','Miss Hurain','Miss Zoha'];
+console.log(`We have ${guest.length} guests for dinner.`);
