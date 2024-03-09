002d3c065aa317434aa9e42a4f5fef269972c953
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:35:22 +0500

    Add files via upload

---
+++ Assignment44/main.js
@@ -0,0 +1,5 @@
+var sandwich = ['Bread slices', 'Filling', 'Condiments'];
+function sandwiches(Bread, Filling, Condiment) { console.log("The sandwich may contain ".concat(Bread, " slices of bread,").concat(Filling, " filling and ").concat(Condiment)); }
+sandwiches(3, 'chicken', 'bbq sauce');
+sandwiches(4, 'meat', 'chilli garlic sauce');
+sandwiches(2, 'veggies', 'mayonise');
---
+++ Assignment44/main.ts
@@ -0,0 +1,6 @@
+let sandwich=['Bread slices','Filling','Condiments'];
+function sandwiches(Bread:number,Filling:string,Condiment:string) 
+{console.log(`The sandwich may contain ${Bread} slices of bread,${Filling} filling and ${Condiment}`)}
+sandwiches(3,'chicken','bbq sauce');
+sandwiches(4,'meat','chilli garlic sauce');
+sandwiches(2,'veggies','mayonise');
