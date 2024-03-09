e6337e8cece889938123a0f4abcd16ab5702b0eb
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 22:45:29 +0500

    Add files via upload

---
+++ Assignment12/main.js
@@ -0,0 +1,4 @@
+var names = ['salma', 'saniya', 'zonu'];
+console.log("Hello ".concat(names[0], ",nice to meet you"));
+console.log("Hello ".concat(names[1], ",nice to meet you"));
+console.log("Hello ".concat(names[2], ",nice to meet you"));
---
+++ Assignment12/main.ts
@@ -0,0 +1,4 @@
+let names: string[] = ['salma','saniya','zonu'];
+console.log(`Hello ${names[0]},nice to meet you`);
+console.log(`Hello ${names[1]},nice to meet you`);
+console.log(`Hello ${names[2]},nice to meet you`);
