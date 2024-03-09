e1c57467cefc9437843429a3aa5cea411f839570
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 19:45:16 +0500

    Add files via upload

---
+++ Assignment03/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let city = 'karachi';
+console.log(city.toUpperCase());
+console.log(city.toLowerCase());
+function TitleCase(city) {
+    city = city.toLowerCase().split(" ");
+    for (let i = 0; i < city.length; i++) {
+        city[i] = city[i].charAt(0).toUpperCase() + city[i].slice(1);
+    }
+    return city.join(" ");
+}
+console.log(TitleCase(city));
