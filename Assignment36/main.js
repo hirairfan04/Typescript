036a84857215e8237a3b32c57018de044b9f371b
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:27:25 +0500

    Add files via upload

---
+++ Assignment36/main.js
@@ -0,0 +1,2 @@
+function make_shirt(size, message) { console.log("Size:".concat(size), "Message:".concat(message)); }
+make_shirt('Medium', 'Do what makes you happy!');
---
+++ Assignment36/main.ts
@@ -0,0 +1,2 @@
+function make_shirt(size:string,message:string) {console.log(`Size:${size}`,`Message:${message}`)}
+make_shirt('Medium','Do what makes you happy!');
