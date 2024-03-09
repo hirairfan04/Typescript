f453c7ff21159a24dccf2b883c77e9a3669f9cca
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:17:54 +0500

    Add files via upload

---
+++ Assignment27/main.js
@@ -0,0 +1,34 @@
+"use strict";
+//it will run the if statement
+let alien_color = 'green';
+if (alien_color == 'green') {
+    console.log('You just earned 5 points');
+}
+else if (alien_color == 'yellow') {
+    console.log('You just earned 10 points');
+}
+else if (alien_color == 'red') {
+    console.log('You just earned 15 points');
+}
+//it will run the 1st else if statement
+let aliencolor = 'yellow';
+if (aliencolor == 'green') {
+    console.log('You just earned 5 points');
+}
+else if (aliencolor == 'yellow') {
+    console.log('You just earned 10 points');
+}
+else if (aliencolor == 'red') {
+    console.log('You just earned 15 points');
+}
+//it will run the 2nd else if statement
+let alienColor = 'red';
+if (alienColor == 'green') {
+    console.log('You just earned 5 points');
+}
+else if (alienColor == 'yellow') {
+    console.log('You just earned 10 points');
+}
+else if (alienColor == 'red') {
+    console.log('You just earned 15 points');
+}
