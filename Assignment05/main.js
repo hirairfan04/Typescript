8dc525be46082b00c7382aa5a8c2f61e3e97d969
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 20:14:17 +0500

    Add files via upload

---
+++ Assignment05/main.js
@@ -0,0 +1,3 @@
+var personality = ['Albert Einstein'];
+var message = ['"A person who never made a mistake never tried anything new."'];
+console.log("".concat(personality, " once said, ").concat(message));
---
+++ Assignment05/main.ts
@@ -0,0 +1,3 @@
+let personality=['Albert Einstein'];
+let message=['"A person who never made a mistake never tried anything new."'];
+console.log(`${personality} once said, ${message}`);
