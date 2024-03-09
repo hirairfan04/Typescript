b23310944c0f03f55804f1b2b3ec5b1232e7d389
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 20:26:40 +0500

    Add files via upload

---
+++ Assignment09/main.js
@@ -0,0 +1,3 @@
+var fvrt_number = 7;
+var message = 'My favorite number is';
+console.log("".concat(message, " ").concat(fvrt_number));
---
+++ Assignment09/main.ts
@@ -0,0 +1,3 @@
+let fvrt_number=7;
+let message= 'My favorite number is';
+console.log(`${message} ${fvrt_number}`);
