09763c00f0b54da3ccc1c11ca72ec8380419fba2
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:30:27 +0500

    Add files via upload

---
+++ Assignment39/main.ts
@@ -0,0 +1,4 @@
+function city_country(city:string,country:string) {console.log(`${city},${country}`)}
+city_country('Karachi','Pakistan');
+city_country('Lahore','Pakistan');
+city_country('Seoul','South Korea');
