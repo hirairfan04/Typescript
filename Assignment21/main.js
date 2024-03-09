2bea04f9bf3dd995a7a6085066224c12d883e96b
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:09:23 +0500

    Add files via upload

---
+++ Assignment21/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let employee = {
+    id: 1,
+    name: "Hira",
+};
+console.log(`Id:${employee.id}, Name:${employee.name}`);
---
+++ Assignment21/main.ts
@@ -0,0 +1,8 @@
+let employee:{
+    id: number;
+    name: string;
+}={
+    id:1,
+    name:"Hira",
+    }
+console.log(`Id:${employee.id}, Name:${employee.name}`);
