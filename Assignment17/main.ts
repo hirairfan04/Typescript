6214bb3d349882b4c7d85d4987e7245b94261637
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:02:28 +0500

    Add files via upload

---
+++ Assignment17/main.ts
@@ -0,0 +1,23 @@
+//let message=',I would like to invite you at dinner at my place this Saturday at 7pm.'
+//let guest=['Miss Meerab','Miss Abrish','Miss Irtifa','Miss Hurain','Miss Zoha'];
+//let quantity =guest.push('Miss Sana');
+//console.log(guest[0]+message);
+//console.log(guest[1]+message);
+//console.log(guest[2]+message);
+//console.log(guest[3]+message);
+//console.log(guest[4]+message);
+//console.log(guest[5]+message);
+
+console.log("There's a bad news! Our dinner table won't arrive on time so I'll only invite 2 persons for dinner");
+
+let guest=[];
+
+console.log("Miss Meerab, I'm sorry I can't invite you at the dinner");
+console.log("Miss Abrish, I'm sorry I can't invite you at the dinner");
+console.log("Miss Irtifa, I'm sorry I can't invite you at the dinner");
+console.log("Miss Hurain, I'm sorry I can't invite you at the dinner");
+
+console.log("Miss Zoha you're lucky, you're still invited at dinner");
+console.log("Miss Sana you're lucky, you're still invited at dinner");
+
+console.log(guest);
