6ee9b558c5ee404fc79e6889697ee54bc1b9ebbc
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:36:25 +0500

    Add files via upload

---
+++ Assignment45/main.js
@@ -0,0 +1,4 @@
+function car(manufacturer, model_name, color, type) { console.log("Manufacturer:".concat(manufacturer, "\n Model name:").concat(model_name, "\n Color:").concat(color, "\n Is it sedan?").concat(type, "\n")); }
+car('Toyota', 'Corolla', 'White', true);
+car('Kia', 'Sportage', 'Maroon', false);
+car('Toyota', 'Hilux', 'Black', false);
---
+++ Assignment45/main.ts
@@ -0,0 +1,5 @@
+function car(manufacturer:string, model_name:string, color:string, type:boolean) 
+{console.log(`Manufacturer:${manufacturer}\n Model name:${model_name}\n Color:${color}\n Is it sedan?${type}\n`)}
+car('Toyota','Corolla','White',true);
+car('Kia','Sportage','Maroon',false);
+car('Toyota','Hilux','Black',false);
