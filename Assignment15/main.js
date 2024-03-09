2393462f207dbac0a3e25c36daa0fa7beb6983e0
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 22:56:56 +0500

    Add files via upload

---
+++ Assignment15/main.js
@@ -0,0 +1,6 @@
+var message = ',I would like to invite you at dinner at my place this Saturday at 7pm.';
+console.log('Miss Sara');
+var guest = ['Miss Abrish', 'Miss Hurain', 'Miss Zoha'];
+console.log(guest[0] + message);
+console.log(guest[1] + message);
+console.log(guest[2] + message);
---
+++ Assignment15/main.ts
@@ -0,0 +1,9 @@
+//let guest:string[]=['Miss Sara','Miss Hurain','Miss Zoha'];
+let message=',I would like to invite you at dinner at my place this Saturday at 7pm.'
+
+console.log('Miss Sara');
+
+let guest=['Miss Abrish','Miss Hurain','Miss Zoha'];
+console.log(guest[0]+message);
+console.log(guest[1]+message);
+console.log(guest[2]+message);
