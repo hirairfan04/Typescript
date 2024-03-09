e5ba569900444f7364af3cb46955ac10bd141943
hirairfan04 <162793914+hirairfan04@users.noreply.github.com>
2024-03-09 23:19:46 +0500

    Add files via upload

---
+++ Assignment29/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let fvrt_fruits = ['Mango', 'Orange', 'Grapes'];
+if (fvrt_fruits.includes('Apple')) {
+    console.log('You really like bananas');
+}
+if (fvrt_fruits.includes('Mango')) {
+    console.log('You really like mangoes');
+}
+if (fvrt_fruits.includes('Grapes')) {
+    console.log('You really like grapes');
+}
+if (fvrt_fruits.includes('Pineapple')) {
+    console.log('You really like pineapples');
+}
+if (fvrt_fruits.includes('Orange')) {
+    console.log('You really like oranges');
+}
